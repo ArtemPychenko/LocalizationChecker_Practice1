f5f94da0b724a0841cb0e3696df487f5b5b37fa9
Afidah Abdulaziztan <afidah.abdulaziztan@gmail.com>
2019-09-18 22:33:56 +0800

    Add command line parser

--- LocalizationChecker_Practice1/LocalizationChecker_Practice1/Program.cs
+++ LocalizationChecker_Practice1/LocalizationChecker_Practice1/Program.cs
@@ -1,44 +1,101 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
 
 namespace LocalizationChecker_Practice1
 {
+    public class Options
+    {
+        [Option('l', "languages", Separator =';', Required = true, HelpText = "Paths to language files to be processed, separated by semicolon")]
+        public IEnumerable<string> LanguageFiles { get; set; }
+
+        [Option('m', "master", Required = true, HelpText = "Path to master file.")]
+        public string MasterFile { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output report file to be generated.")]
+        public string OutputFile { get; set; }
+
+        [Option('f', "format", Required = true, HelpText = "Report output format.  Supported formats=Text")]
+        public ReportOutputFormat OutputFormat { get; set; }
+
+        [Option('p', "path filter", Required = false, HelpText = "Path filter.")]
+        public string PathFilter { get; set; }
+    }
+
+    public enum ReportOutputFormat
+    {
+        Text   
+    }
+
     class Program
     {
-        static string englishFile = @"C:\Users\Afidah\Desktop\keys.xml";
-        static string denmarkFile = @"C:\Users\Afidah\Desktop\keys-denmark.xml";
-
-
         static void Main(string[] args)
         {
-            FileReader reader = new FileReader();
-            File EnglishFile = reader.ReadXMLFile(englishFile);
-            File DenmarkFile = reader.ReadXMLFile(denmarkFile);
-            var selectedPath = "/sitecore/client/Applications/MarketingAutomation";
-
-            Checker checker = new Checker();
-            var result = checker.Check(EnglishFile, new File[] { DenmarkFile }, selectedPath);
-
-            TextReportGenerator reportGenerator = new TextReportGenerator();
-            var report = reportGenerator.Generate(result);
-
-            using (StreamReader streamReader = new StreamReader(report))
+            Parser parser = new Parser(settings =>
             {
-                var reportContent = streamReader.ReadToEnd();
-                Console.WriteLine(reportContent);
+                settings.HelpWriter = Console.Error;
+                settings.CaseInsensitiveEnumValues = true;
+            });
 
-            }
-            
-                Console.WriteLine("Message End !");
-                Console.ReadKey();
-            
+            parser.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    Console.WriteLine($"Languages = {string.Join(",", o.LanguageFiles)}, ");
+                    Console.WriteLine($"Master Language = {o.MasterFile}");
+                    Console.WriteLine($"Path filter = {o.PathFilter} ");
+                    Console.WriteLine($"Output file = {o.OutputFile} ");
+                    Console.WriteLine($"Output format = {o.OutputFormat} ");
+                    Console.WriteLine($"#################################");
+
+                    if (!System.IO.File.Exists(o.MasterFile))
+                    {
+                        // Ooops, master file does not exist!
+                        Console.Error.WriteLine($"ERROR: Master file '{o.MasterFile}' does not exist");
+                        return;
+                    }
+
+                    FileReader reader = new FileReader();
+                    var masterFile = reader.ReadXMLFile(o.MasterFile);
+
+                    List<File> languageFiles = new List<File>();
+                    foreach (var path in o.LanguageFiles)
+                    {
+                        if (!System.IO.File.Exists(path))
+                        {
+                            // Ooops, language file does not exist!
+                            Console.Error.WriteLine($"ERROR: Langauge file '{path}' does not exist");
+                            return;
+                        }
+
+                        var file = reader.ReadXMLFile(path);
+                        languageFiles.Add(file);
+                    }
+
+                    Checker checker = new Checker();
+                    var result = checker.Check(masterFile, languageFiles, o.PathFilter);
+
+                    TextReportGenerator reportGenerator = new TextReportGenerator();
+                    var report = reportGenerator.Generate(result);
+
+                    using (StreamReader streamReader = new StreamReader(report))
+                    {
+                        var reportContent = streamReader.ReadToEnd();
+
+                        // 1 To create file stream (write and read)
+                        using (FileStream outputFile = new FileStream(o.OutputFile, FileMode.Create, FileAccess.Write))
+                        {
+                            // 2 To write to file stream 
+                            using (StreamWriter writer = new StreamWriter(outputFile))
+                            {
+                                writer.Write(reportContent);
+                            }
+                        }
+                    }
+
+                    Console.WriteLine("Message End !");
+                });
         }
-
-        
     }
 }
